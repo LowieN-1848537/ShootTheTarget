d12402188d272355f1ff5e5665950baeedbc528d
3low1 <60964789+3low1@users.noreply.github.com>
2022-10-09 21:46:30 +0200

    duck turn on wall hit

--- Assets/Duck_materials/duck_movement.cs
+++ Assets/Duck_materials/duck_movement.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 public class duck_movement : MonoBehaviour
 {
     [SerializeField]
+    public GameObject duck;
     private Rigidbody m_body;
     float m_speed;
+    bool rotate = false;
+    private void OnCollisionEnter(Collision collision){
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "wall"){
+        rotate = true;
+        }
+        
+
+    }
+
+
+
+
+
+
     void Start()
     {
-        
+        duck = GameObject.Find("duck");
         m_body = GetComponent<Rigidbody>();
-        m_speed = 0.5f;
+        m_speed = 0.3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_body.velocity = -transform.right * m_speed;
+        m_body.velocity = - transform.right * m_speed;
+        if (rotate){
+            duck.transform.Rotate(0,90,0);
+            rotate = false;
+            Debug.Log("turned");
+        }
     }
 }
